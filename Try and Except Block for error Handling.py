9e87f858591260ec9792e39e461d1450caba130d
TarakaKoda <taraka.koda@gmail.com>
2023-04-04 15:31:20 +0530

    added an example

--- Try and Except Block for error Handling.py
+++ Try and Except Block for error Handling.py
@@ -1,4 +1,14 @@
-try :
-    pass
-except Exception:
-    pass
+try:
+    f = open("example_file.py")
+    # bad = bad_var
+# except FileNotFoundError:
+#     print("sorry! file not found")
+# except Exception:
+#     print("opps! something went wrong")
+except FileNotFoundError as a:
+    print("sorry! this file is not found")
+    print(a)
+except NameError as e:
+    print(e)
+
+
